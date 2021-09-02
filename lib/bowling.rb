5342c5f5f80bcb70c651519ce3fd7291b4a1bb56
zainab baruud <zainabbaruud@ma373.home>
2021-09-02 12:17:05 +0100

    adding the scores of 20 roles

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -1,9 +1,25 @@
 class Bowling
-  def roll
-    
+
+  def initialize(rolls = [])
+    @rolls = []
+    @score = 0
   end
 
-  def score
-    0
+  def roll(pins)
+    @rolls << pins
   end
+
+  def score_total
+    roll_count = 0
+    #roll_count tells @rolls array the number of times we keep going
+    #roll_count is the positiion we are in e.g. index[3] is the 4th roll
+    20.times { @score += @rolls[roll_count] 
+              roll_count += 1 }
+    @score
+  end
+
+
 end
+game = Bowling.new
+
+
--- spec/bowling_spec.rb
+++ spec/bowling_spec.rb
@@ -1,8 +1,17 @@
 require 'bowling'
+
 describe Bowling do
+
   it 'should roll a gutter game' do
     game = Bowling.new
-    20.times { game.roll }
-    expect(game.score).to eq 0
+    20.times { game.roll(0) }
+    expect(game.score_total).to eq 0
   end
 
+  it 'should get a score of 1 in each roll' do
+    game = Bowling.new
+    20.times { game.roll(1) }
+    expect(game.score_total).to eq 20
+  end
+
+end
